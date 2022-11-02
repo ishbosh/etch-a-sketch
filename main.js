4d16a0b73fe8f7f5b49cf482fecbef9f1406b1b7
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-11-02 11:40:17 -0700

    Set up initial pseudocode comments

---
+++ main.js
@@ -0,0 +1,4 @@
+// Create a 16x16 grid of square divs
+// Insert divs into main div container
+// Add a Hover effect to the grid divs to change the color on mouseover, leaving a pixelated trail
+// Add a button to the top of the screen that will send a popup asking for the number of squares per side for a new grid. Max 100. Fit to same total space.
