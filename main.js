abadbd15141f86ed58f5f879d47be35126d694d7
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-11-02 11:52:01 -0700

    Update pseudocode comments second pass

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 // Create a 16x16 grid of square divs
+    // Given GRID_SIZE, create GRID_SIZE number of rows of with GRID_SIZE number of div elements in each row.
+    // Assign class of grid to each div element
+
 // Insert divs into main div container
+    // insert all div elements with class of grid into div with class of container
+
 // Add a Hover effect to the grid divs to change the color on mouseover, leaving a pixelated trail
+    // Add event listener to each div element with class of grid to listen for mouseover
+    // Change background color of the div mouse has entered when mouse enters each div
+
 // Add a button to the top of the screen that will send a popup asking for the number of squares per side for a new grid. Max 100. Fit to same total space.
+    // Add button to HTML file to create new grid
+    // Use prompt to request new grid size, set limit between 16 - 100
+    // Limit the original div container size so they fit into the same space.
+
